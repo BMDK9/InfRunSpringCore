ab8d761207823f1d7a6c56a2c77df5a945f7055a
BMDK9 <qnrakdlzm77@naver.com>
2024-04-10 17:58:09 +0900

    feat : Lombok 적용

--- src/main/java/hello/spring_core/member/service/impl/MemberServiceImpl.java
+++ src/main/java/hello/spring_core/member/service/impl/MemberServiceImpl.java
@@ -3,18 +3,22 @@ package hello.spring_core.member.service.impl;
 import hello.spring_core.member.entity.Member;
 import hello.spring_core.member.repository.MemberRepository;
 import hello.spring_core.member.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+@Getter
 @Component
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
+    // 테스트 용도
     private final MemberRepository memberRepository;
 
-    @Autowired
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+//    @Autowired
+//    public MemberServiceImpl(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
     @Override
     public void signIn(Member member) {
@@ -26,8 +30,4 @@ public class MemberServiceImpl implements MemberService {
         return memberRepository.findById(memberId);
     }
 
-    // 테스트 용도
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
 }
