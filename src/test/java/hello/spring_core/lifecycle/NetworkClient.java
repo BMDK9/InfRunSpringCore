b0dd8c7034dfe612a335d41663ce9f52a5ba3928
BMDK9 <qnrakdlzm77@naver.com>
2024-04-11 13:51:15 +0900

    feat : 빈 등록으로 초기화 소멸 메서드

--- src/test/java/hello/spring_core/lifecycle/NetworkClient.java
+++ src/test/java/hello/spring_core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.spring_core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -29,18 +26,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close = " + url);
     }
 
-
-    // 아래의 afterPropertiesSet, destroy 는 잘 사용하지 않는 방법이다.
-    @Override
-    public void afterPropertiesSet() throws Exception { // 의존 관계 주입이 끝나면 ~ 해주겠다
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
