3ec90e21bb61000760feba45a2d7931b90318325
BMDK9 <qnrakdlzm77@naver.com>
2024-04-16 10:43:22 +0900

    feat : ObjectProvider

--- src/test/java/hello/spring_core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/spring_core/scope/SingletonWithPrototypeTest1.java
@@ -6,6 +6,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -38,13 +39,14 @@ public class SingletonWithPrototypeTest1 {
     @Scope("singleton")
     static class ClientBean {
 
-        private final PrototypeBean prototypeBean; // 생성시점에 주입 완료, 싱글톤이 관리해서 프로토타입 의도대로 작동 X
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
@@ -80,6 +82,6 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 }
